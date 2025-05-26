e124dc3839a613a6b430d2b9c4c54bac964e55b8
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-26 20:08:13 +0900

    udpate

--- app/api/memo/remove/route.ts
+++ app/api/memo/remove/route.ts
@@ -4,15 +4,10 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
     // メモ index 取得
     const { index } = await req.json();
-    console.log("index:", index);
-
     // MemoRepository.removeMemo() で、メモを削除
     removeMemo(index);
-
     // レスポンスデータの作成
-    const result = {
-        message: "メモを削除しました",
-    };
+    const result = { message: "メモを削除しました", index: index, };
     // レスポンス
     return NextResponse.json(result);
 }
