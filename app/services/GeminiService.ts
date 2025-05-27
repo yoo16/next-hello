e7443e30a360d9a823f949d4c31b533b5aa0628c
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-28 06:28:29 +0900

    update

--- app/services/GeminiService.ts
+++ app/services/GeminiService.ts
@@ -1,10 +1,16 @@
-import { GoogleGenerativeAI, } from '@google/generative-ai';
+import { GoogleGenerativeAI } from '@google/generative-ai';
 
 import { ROLE_BOT } from '@/app/constants/roles';
 import { Message } from '@/app/interfaces/Message';
 
 const API_KEY = process.env.GEMINI_API_KEY;
 const GEMINI_MODEL = 'gemini-1.5-flash';
+const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 1024,
+};
 
 export async function sendToGemini(
     userMessage: Message
@@ -13,8 +19,9 @@ export async function sendToGemini(
 
     const genAI = new GoogleGenerativeAI(API_KEY);
     const model = genAI.getGenerativeModel({ model: GEMINI_MODEL });
+    model.generationConfig = generationConfig;
 
-    const prompt = userMessage.content.trim();
+    const prompt = `つぎの質問に答えて。\n\n${userMessage.content}`;
     const result = await model.generateContent(prompt);
     const text = result.response.text().replaceAll('*', '\n');
 
